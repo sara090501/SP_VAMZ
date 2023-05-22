56dd426b7fb45b8d3464685ffffb5c0d023fccdd
Sara Geratova <geratova@isw.sk>
2023-05-22 15:05:30 +0200

    activity_main.xml upgraded

--- app/src/main/java/com/example/kvizzz/MainActivity.kt
+++ app/src/main/java/com/example/kvizzz/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.kvizzz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+    var isImageExpanded = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val logo: ImageView = findViewById(R.id.logo);
+        val playButton: Button = findViewById(R.id.play)
+        playButton.setOnClickListener {
+            if (isImageExpanded) {
+                // Reduce the image size
+                val layoutParams = logo.layoutParams
+                layoutParams.width /= 2
+                layoutParams.height /= 2
+                logo.layoutParams = layoutParams
+            } else {
+                // Increase the image size
+                val layoutParams = logo.layoutParams
+                layoutParams.width *= 2
+                layoutParams.height *= 2
+                logo.layoutParams = layoutParams
+            }
+
+            isImageExpanded = !isImageExpanded
+        }
     }
 }
---
+++ app/src/main/java/com/example/kvizzz/MainPage.kt
@@ -0,0 +1,5 @@
+package com.example.kvizzz
+
+class MainPage {
+
+}
