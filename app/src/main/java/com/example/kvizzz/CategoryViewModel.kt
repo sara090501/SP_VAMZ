1e18d667dcf108ea5ae26014e5e5f3e802171198
Sara Geratova <geratova@isw.sk>
2023-06-07 14:59:08 +0200

    starting with views

---
+++ app/src/main/java/com/example/kvizzz/CategoryViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.kvizzz
+
+import androidx.lifecycle.ViewModel
+
+class CategoryViewModel : ViewModel() {
+    private var name = "category"
+    private var description = "description"
+}
