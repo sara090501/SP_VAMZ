7e9138915fc280fafee7779498385f38dbdeb540
geratova.sara <geratova.sara@gmail.com>
2023-06-10 14:05:01 +0200

    trying to run database vol. 3

--- app/src/main/java/com/example/kvizzz/CategoryViewHolder.kt
+++ app/src/main/java/com/example/kvizzz/CategoryViewHolder.kt
@@ -12,23 +12,23 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 
-class CategoryViewHolder private constructor(itemView: View) :
-    RecyclerView.ViewHolder(itemView) {
-    private val categoryItemView: TextView
-
-    init {
-        categoryItemView = itemView.findViewById(R.id.name)
-    }
-
-    fun bind(text: String?) {
-        categoryItemView.text = text
-    }
-
-    companion object {
-        fun create(parent: ViewGroup): CategoryViewHolder {
-            val view: View = LayoutInflater.from(parent.context)
-                .inflate(R.layout.activity_list_item, parent, false)
-            return CategoryViewHolder(view)
-        }
-    }
-}
+//class CategoryViewHolder private constructor(itemView: View) :
+//    RecyclerView.ViewHolder(itemView) {
+//    private val categoryItemView: TextView
+//
+//    init {
+//        categoryItemView = itemView.findViewById(R.id.textView)
+//    }
+//
+//    fun bind(text: String?) {
+//        categoryItemView.text = text
+//    }
+//
+//    companion object {
+//        fun create(parent: ViewGroup): CategoryViewHolder {
+//            val view: View = LayoutInflater.from(parent.context)
+//                .inflate(R.layout.activity_list_item, parent, false)
+//            return CategoryViewHolder(view)
+//        }
+//    }
+//}
