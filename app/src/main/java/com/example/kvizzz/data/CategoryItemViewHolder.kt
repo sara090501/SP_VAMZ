919a7d7aa76eb06a170a42c931bb7ef5fa6ad238
Sara Geratova <geratova@isw.sk>
2023-06-12 20:28:06 +0200

    refactoring

--- app/src/main/java/com/example/kvizzz/data/CategoryItemViewHolder.kt
+++
@@ -1,7 +0,0 @@
-package com.example.kvizzz.data
-
-import android.widget.TextView
-import androidx.recyclerview.widget.RecyclerView
-
-class CategoryItemViewHolder(val textView: TextView): RecyclerView.ViewHolder(textView) {
-}
